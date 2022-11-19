96d26edeb531a048447ed34f88d9a0d7ff8aed34
gk-svg <gk32239@gmail.com>
2022-11-19 19:14:30 +0530

    text utills advance

--- page/views.py
+++ page/views.py
@@ -15,8 +15,8 @@ def puncation(request):
     remover=request.POST.get('lineremover','off')
     spacetype=request.POST.get('spaceremover','off')
     count=request.POST.get('countword','off')
-    #return HttpResponse("remove punc")
-    #analyzed=djtext
+    context = {}
+    
     if remove == "on":
         punctuations="""!"#$%&'()*+, -./:;<=>?@[\]^_`{|}~"""
         analyzed=""
@@ -24,24 +24,23 @@ def puncation(request):
             if char not in punctuations:
                 analyzed=analyzed+char
         params={'purpose':'removedpuncation','analyzed_text':analyzed}
-        djtext=analyzed
-        #return render(request,'sync.html',params)
+        djtext = analyzed
+       
     if(fullcap=="on"):
         syncword=""
         for char in djtext:
             syncword=syncword+char.upper()
         scoop={'purpose':'uppercase','printtype':syncword}
-        djtext=syncword
-        #return render(request,'sync.html',scoop)
-
+        djtext = syncword
+        
     if(remover=="on"):
         line=""
         for char in djtext:
             if char!='\n' and char!='\r':
                 line=line + char
         pogo={'purpose':'lineremover','effect':line}
-        djtext=line
-        #return render(request,'sync.html',pogo)
+        djtext = line
+        
     if(spacetype=='on'):
         spoke=""
         for index,char in enumerate(djtext):
@@ -49,16 +48,15 @@ def puncation(request):
                 
                 spoke=spoke+char
         past={'purpose':'removedline','effectthis':spoke}
-        djtext=spoke
-        #return render(request,'sync.html',past)
+        djtext = spoke
+        
     if(count=="on"):
         code=0
         for index in range(len(djtext)):
             if not(djtext[index]==" "):
                 code=code+1
-        sep={'purpose':"countwords",'typeshit':code}
-        djtext=code
-        #return render(request,'sync.html',sep)
-        return render(request,'sync.html',djtext)               
-    else:
-        return HttpResponse("error")          
+        context['code'] = code
+        
+    context['djtext'] = djtext
+    return render(request,'sync.html',context)               
+        
